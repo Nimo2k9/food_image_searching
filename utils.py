9f3a4fee39d3c8785c53b3235ab2fc1f6fcb841b
Nimo2k9 <148305782+Nimo2k9@users.noreply.github.com>
2026-04-15 00:33:35 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ genai.configure(api_key=get_secret("GEMINI_API_KEY"))
 # -------------------------------
 def detect_food(image_file):
     try:
-        model = genai.GenerativeModel("gemini-2.5-pro")
+        model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
         image_bytes = image_file.read()
 
